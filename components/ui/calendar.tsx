0e097dc8f2b2c4ecb32dd553fb2f4839ea4aa262
Nurmurod23 <elow9020@gmail.com>
2024-11-03 11:43:53 +0500

    aaaa

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -2,7 +2,7 @@
 
 import * as React from 'react';
 import { ChevronLeft, ChevronRight } from 'lucide-react';
-import { DayPicker } from 'react-day-picker';
+import DayPicker from 'react-day-picker';
 
 import { cn } from '@/lib/utils';
 import { buttonVariants } from '@/components/ui/button';
@@ -12,20 +12,18 @@ export type CalendarProps = React.ComponentProps<typeof DayPicker>;
 function Calendar({
   className,
   classNames,
-  showOutsideDays = true,
   ...props
 }: CalendarProps) {
   return (
     <DayPicker
-      showOutsideDays={showOutsideDays}
+      showOutsideDays
       className={cn('p-3', className)}
       classNames={{
-        months: 'flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0',
-        month: 'space-y-4',
+        months: 'space-y-4',
         caption: 'flex justify-center pt-1 relative items-center',
-        caption_label: 'text-sm font-medium',
+        captionLabel: 'text-sm font-medium',
         nav: 'space-x-1 flex items-center',
-        nav_button: cn(
+        navButton: cn(
           buttonVariants({ variant: 'outline' }),
           'h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100'
         ),
